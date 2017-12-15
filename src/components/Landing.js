956c9301e759684284e2459da859d8e84e3390b2
Russell Hayes <info@rahphotographyco.com>
2017-12-15 14:29:20 -0700

    brbwtbgt

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,6 +6,11 @@ class Landing extends React.Component {
         return (
             <div className='landing-main'>
                 <div className='hero-image-landing' alt=''>
+                    <div className='filmshare-text-hero' >FILMSHARE</div>
+                    <div className='buttons-landing-main' >
+                        <button to='/dashboard' className='login-button-landing' >LOGIN</button>
+                        <button to='/dashboard' className='create-account-landing' >CREATE ACCOUNT</button>
+                    </div>
                 </div>
             </div>
         )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../assets/Octicons-device-camera.svg';
 
 class Navbar extends React.Component {
     render() {
@@ -7,7 +8,7 @@ class Navbar extends React.Component {
             <div>
                 <div className='navbar-main' >
                     <Link to='/'>
-                    <div className='nav-logo' ></div>
+                    <img className='nav-logo' src={logo} />
                     </Link>
                     <Link to='/dashboard'>Home</Link>
                     <Link to='/'>Search</Link>
